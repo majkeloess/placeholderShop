3e68ded19a054dbd76b23c356a202e3e45b380e5
majkeloess <msaturczak@student.agh.edu.pl>
2024-09-25 13:43:39 +0200

    Added new dependencies & homepage UI

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -12,6 +12,36 @@ class HomepageController extends AbstractController
   public function homepage()
   {
     $placeholder = "{{placeholder}}";
-    return $this->render("homepage/homepage.html.twig", ['placeholder' => $placeholder]);
+    $items = [
+      [
+        "id" => "1",
+        "name" => "Logo tee",
+        "image" => "img/tee.png",
+        "price" => "89$",
+      ],
+      [
+        "id" => "2",
+        "name" => "Constructor hoodie",
+        "image" => "img/hoodie.png",
+        "price" => "129$",
+      ],
+      [
+        "id" => "3",
+        "name" => "spider zip",
+        "image" => "img/zip.png",
+        "price" => "129$",
+      ],
+      [
+        "id" => "4",
+        "name" => "Bird sleveless",
+        "image" => "img/sleveless.png",
+        "price" => "69$",
+      ],
+    ];
+
+    return $this->render("homepage/homepage.html.twig", [
+      'placeholder' => $placeholder,
+      'items' => $items
+    ]);
   }
 }
