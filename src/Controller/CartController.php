9a385cae740855ec5a138b2f2173002e35db6892
majkeloess <msaturczak@student.agh.edu.pl>
2024-09-26 11:54:10 +0200

    Added session, cart page implementation

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -1,15 +1,44 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-  #[Route("/cart")]
-  public function cart()
+
+  #[Route("/cart", "cart")]
+  public function cart(SessionInterface $session)
   {
-    return $this->render("/cart/cart.html.twig");
+    $cart = $session->get('cart');
+
+    return $this->render("/cart/cart.html.twig", ["placeholder" => "{{placeholder}}", "cart" => $cart]);
+  }
+
+  #[Route("/cart/add", "cart_add")]
+  public function add(Request $request, SessionInterface $session, ProductRepository $product)
+  {
+    $id = $request->request->get("product_id");
+    $size = $request->request->get("size");
+
+    $cart = $session->get('cart');
+
+    $prod = $product->fetchById($id);
+
+    $cart[$id] = [
+      "name" => $prod->getName(),
+      "size" => $size,
+      "price" => $prod->getPrice(),
+      "image" => $prod->getImage(),
+      "quantity" => 1
+    ];
+
+    $session->set('cart', $cart);
+
+    return $this->redirectToRoute("cart");
   }
 
 
